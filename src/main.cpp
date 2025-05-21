e5452f34d2af905e47b852b4cdcbd6a5da8d6093
TimoStam <23099968@student.hhs.nl>
2025-05-22 00:14:10 +0200

    test: i2c data transfer test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <string>
 
 #include "BusServer.h"
+#include "SlaveManager.h"
+#include "RGBSlave.h"
 
 int main() {
     BusServer bus_server;
+    SlaveManager slave_manager;
+    RGBSlave rgb_slave;
+    sensor_packet_rgb_light packet;
+    packet.metadata.sensor_type = SensorType::RGB_LIGHT;
+    packet.metadata.sensor_id = id;
+    packet.red_state = 255;
+    packet.green_state = 0;
+    packet.blue_state = 100;
 
     bus_server.setup("0.0.0.0", 5000);
     bus_server.start();
+    slave_manager.createSlave(RGB_LIGHT, 0x80, 0x55);
+    rgb_slave = slave_manager.getSlave(0x80);
+    rgb_slave.setData(&packet);
 }
