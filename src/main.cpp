18bebe49cc3245a3c7276e2edb99c01ed7150a47
TimoStam <23099968@student.hhs.nl>
2025-05-22 00:39:44 +0200

    fix: sensor_id, changed rgb_slave assignment

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 
+#include "packets.h"
 #include "BusServer.h"
 #include "SlaveManager.h"
 #include "RGBSlave.h"
@@ -7,10 +8,9 @@
 int main() {
     BusServer bus_server;
     SlaveManager slave_manager;
-    RGBSlave rgb_slave;
     sensor_packet_rgb_light packet;
     packet.metadata.sensor_type = SensorType::RGB_LIGHT;
-    packet.metadata.sensor_id = id;
+    packet.metadata.sensor_id = 0x80;
     packet.red_state = 255;
     packet.green_state = 0;
     packet.blue_state = 100;
@@ -18,6 +18,6 @@ int main() {
     bus_server.setup("0.0.0.0", 5000);
     bus_server.start();
     slave_manager.createSlave(RGB_LIGHT, 0x80, 0x55);
-    rgb_slave = slave_manager.getSlave(0x80);
+    RGBSlave rgb_slave = slave_manager.getSlave(0x80);
     rgb_slave.setData(&packet);
 }
