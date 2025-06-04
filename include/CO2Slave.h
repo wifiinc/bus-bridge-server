9ca62bd64b62f02b3734417825aaedf022f50758
Timo <timo.stam2004@gmail.com>
2025-06-04 17:17:53 +0200

    implemented: co2slave | needs testing and stm code for co2 sensor

--- include/CO2Slave.h
+++ include/CO2Slave.h
@@ -3,18 +3,25 @@
 
 #include "BaseSlave.h"
 
+struct CO2Data {
+    uint16_t value;
+}__attribute__((packed));
+
 class CO2Slave : public BaseSlave {
    public:
-    CO2Slave(uint8_t id, int i2c_address);
+    CO2Slave(uint8_t id, uint8_t i2c_address);
 
-    void* getData() override;
-    bool getStatus() override;
-    int getId() override;
-    void setData(void* data) override;
-    void start(int i2c_fd) override;
-    void stop() override;
+    void* getData();
+    bool getStatus();
+    int getId();
+    void setData(void* data);
+    void start(int i2c_fd);
+    void stop();
 
    private:
     int id;
     int fd;
+    uint8_t i2c_address;
+
+    CO2Data co2data;
 };
--- src/CO2Slave.cpp
+++ src/CO2Slave.cpp
@@ -1,19 +1,35 @@
 #include "CO2Slave.h"
 
-CO2Slave::CO2Slave(uint8_t id, int i2c_address) : id(id) {
-    /*
-     * Open I2C connection and save the FD in the 'fd' member variable
-     */
+CO2Slave::CO2Slave(uint8_t id, uint8_t i2c_address) : id(id), i2c_address(i2c_address) {}
+
+void* CO2Slave::getData() {
+    uint16_t value;
+
+    value = wiringPiI2CRead(fd);
+    
+    co2data.value = value;
+    
+    return; 
 }
 
-void* CO2Slave::getData() { return nullptr; }
-
-bool CO2Slave::getStatus() { return nullptr; }
+bool CO2Slave::getStatus() {
+    getData();
+    if (co2data.value == 0){
+        return false;
+    } else {
+        return true;
+    }
+}
 
 int CO2Slave::getId() { return id; }
 
-void CO2Slave::setData(void* data) {}
+void CO2Slave::setData(void* data) {
+    CO2Data* co2_data = (CO2Data*)data;
+    uint16_t value = co2_data->value;
 
-void CO2Slave::start(int i2c_fd) {}
+    wiringPiI2CWrite(fd, value);
+}
 
-void CO2Slave::stop() {}
+void CO2Slave::start(int i2c_fd) { fd = i2c_fd; }
+
+void CO2Slave::stop() { fd = -1; }
